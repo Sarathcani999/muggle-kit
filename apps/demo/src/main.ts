192746538be7961120e6b4b39b2f6db04295ba68
Sarath C Ani <mailsforsarath98@gmail.com>
2025-04-18 10:48:42 +0530

    [infra]: setup folder structure core lib, demo ui

---
+++ apps/demo/src/main.ts
@@ -0,0 +1,9 @@
+import { add } from "@repo/core/add";
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <p>
+      Edit <code>apps/demo/src/main.ts</code> and save to reload.
+    </p>
+  </div>
+`;
