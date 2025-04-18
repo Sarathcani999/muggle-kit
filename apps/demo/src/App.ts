99214d4ea5d77a20092af9ddcadfc47f565e0181
Sarath C Ani <mailsforsarath98@gmail.com>
2025-04-18 23:19:41 +0530

    chore(demo): update folder structure of components

--- apps/demo/src/App.ts
+++ apps/demo/src/App.ts
@@ -1,6 +1,6 @@
 import { h, hFragment } from "@repo/core/mugglekit";
-import CreateTodo from "./CreateTodo";
-import TodoList from "./TodoList";
+import CreateTodo from "./components/CreateTodo";
+import TodoList from "./components/TodoList";
 
 const App = (state: any) => {
   return hFragment([h("h1", {}, ["My TODOs"]), CreateTodo(), TodoList(state)]);
--- apps/demo/src/main.ts
+++ apps/demo/src/main.ts
@@ -4,6 +4,6 @@ import App from "./App";
 const root = document.querySelector<HTMLDivElement>("#root");
 
 if (root) {
-  const { mount } = createApp({state: { todos: [] }, view: App})
+  const { mount } = createApp({state: { todos: ["Clean the room"] }, view: App})
   mount(root);
 }
