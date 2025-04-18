ab33b129aed552f541aa6091c83644d152dfc739
Sarath C Ani <mailsforsarath98@gmail.com>
2025-04-18 10:55:09 +0530

    [chore]: format codebase

--- apps/demo/src/main.ts
+++ apps/demo/src/main.ts
@@ -1,4 +1,4 @@
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
     <p>
       Edit <code>apps/demo/src/main.ts</code> and save to reload.
