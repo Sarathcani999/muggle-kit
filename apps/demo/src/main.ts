5a0ac2d2c1c98b12b9e41cf4a7d49bf8a309b95c
Sarath C Ani <mailsforsarath98@gmail.com>
2025-04-18 15:09:15 +0530

    feat: update the template and main.ts file to invoke core lib and functionality

--- apps/demo/src/main.ts
+++ apps/demo/src/main.ts
@@ -1,7 +1,13 @@
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    <p>
-      Edit <code>apps/demo/src/main.ts</code> and save to reload.
-    </p>
-  </div>
-`;
+import { h } from "@repo/core/mugglekit";
+
+const root = document.querySelector<HTMLDivElement>("#root");
+
+if (root) {
+  const form = h("form", { class: "login-form", action: "login" }, [
+    h("input", { type: "text", name: "username" }),
+    h("input", { type: "password", name: "password" }),
+    h("button", { type: "submit" }, ["Login"]),
+  ]);
+
+  console.log(form);
+}
