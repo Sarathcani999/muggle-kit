61a0175e832248939452b286e361e988da619bfe
Sarath C Ani <mailsforsarath98@gmail.com>
2025-05-23 21:22:27 +0530

    chore: remove demo application files and components

--- apps/demo/src/components/TodoItem.ts
+++
@@ -1,7 +0,0 @@
-import { h } from "@sarathcani999/mugglekit";
-
-const TodoItem = (todo: string, idxInList: number) => {
-  return h("li", {}, [`No. ${idxInList}: ${todo}`]);
-};
-
-export default TodoItem;
